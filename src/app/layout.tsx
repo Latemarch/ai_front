c186993187660dc9bc1923c464185a16b4b76144
jun <junhyeongjoen@junui-Macmini.local>
2025-09-05 18:10:28 +0900

    tailwind

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <div className="fixed inset-0 bg-red-100"></div>
         {children}
       </body>
     </html>
