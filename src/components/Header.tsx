6279fa281adf9c21351c99b7de3eaf1fe27f8a33
jun <junhyeongjoen@junui-Macmini.local>
2025-09-09 11:22:13 +0900

    modify eslint config

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import LanguageSelector from "./LanguageToggle";
 
 export default function Header() {
   return (
-    <div className="flex h-16 shrink-0 text-white font-semibold items-center justify-between px-6 top-0 w-full z-50 bg-black/20 backdrop-blur-md border-b border-white/10">
+    <div className="flex h-16 shrink-0 text-white font-semibold items-center justify-center px-6 top-0 w-full z-50 bg-black/20 backdrop-blur-md border-b border-white/10">
       {/* Navigation Links */}
       <div className="flex items-center gap-10">
         <Link prefetch={false} href="/">
@@ -16,9 +16,9 @@ export default function Header() {
         <div className="cursor-pointer p-2">DASHBOARD</div>
         <div className="cursor-pointer p-2">PRICING</div>
       </div>
-      
+
       {/* Language Selector */}
-      <div className="relative">
+      <div className="flex absolute right-5">
         <LanguageSelector />
       </div>
     </div>
